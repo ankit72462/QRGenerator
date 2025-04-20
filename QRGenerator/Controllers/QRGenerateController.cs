c19f1b529ec3967a60cecbfc89979caa00de4cdc
Ankit Kumar <ankit4205@gmail.com>
2025-04-20 08:43:20 +0530

    Add project files.

---
+++ QRGenerator/Controllers/QRGenerateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QRGenerator.Controllers
+{
+    public class QRGenerateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ QRGenerator/Models/QRCodeModel.cs
@@ -0,0 +1,6 @@
+﻿namespace QRGenerator.Models
+{
+    public class QRCodeModel
+    {
+    }
+}
